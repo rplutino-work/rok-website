1ee1df0a7f826406c695208dd177a57b562ee277
Rodrigo <rodrigo@pow.la>
2023-02-21 01:19:40 -0300

    fix mailchimp

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,7 +6,6 @@ import 'rc-drawer/assets/index.css';
 import 'react-modal-video/css/modal-video.min.css';
 import 'typeface-dm-sans';
 
-
 export default function CustomApp({ Component, pageProps }) {
   useEffect(() => {
     initGA();
